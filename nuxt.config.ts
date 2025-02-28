1182d1fcefc2295c106c81ed57a78804dab59ca4
dungnv05 <dungnv005@gmail.com>
2025-02-28 14:36:03 +0700

    setup baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: [
-    'bootstrap/dist/css/bootstrap.css'
-  ],
+  css: ["bootstrap/dist/css/bootstrap.css"],
   script: [
     {
-      src: 'bootstrap/dist/js/bootstrap.bundle.min.js'
-    }
+      src: "bootstrap/dist/js/bootstrap.bundle.min.js",
+    },
   ],
   build: {
     postcss: {
       plugins: {
-        'postcss-import': {},
-        'postcss-url': {},
-        'autoprefixer': {}
-      }
-    }
+        "postcss-import": {},
+        "postcss-url": {},
+        autoprefixer: {},
+      },
+    },
   },
-  target: 'static', // Xuất trang tĩnh phù hợp với GitHub Pages
+  target: "static", // Xuất trang tĩnh phù hợp với GitHub Pages
   router: {
-    base: '/trangdai-makeup/' // Cập nhật đường dẫn repo của bạn
-  }
-})
+    base: "/trangdai-makeup/", // Cập nhật đường dẫn repo của bạn
+  },
+  app: {
+    baseURL: "/trangdai-makeup/"
+  },
+});
